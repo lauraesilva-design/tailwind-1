589f749a0abafff789c412e4f6d29bdffe99ed6a
lauraesilva-design <laura.e.silva@aluno.senai.br>
2026-02-24 14:00:32 -0300

    Add files via upload

---
+++ app/projects/page.tsx
@@ -0,0 +1,16 @@
+export default function projects(){
+    return(
+        <main className="h-screen w-screen bg-amber-950
+        flex justify-center">
+            <h1>Meus Projetos</h1>
+
+        </main>
+   
+    );
+       
+
+
+        
+          
+          
+} 
